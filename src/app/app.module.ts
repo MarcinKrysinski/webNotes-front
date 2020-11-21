8d057d0cdba396a55591b1e8c2f90a6663654442
Marcin <marcin.jakubkrysinski@gmail.com>
2020-11-21 20:02:12 +0100

    added project structure and get, add, update, delete functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NotesComponent } from './notes/notes.component';
+import { NoteDetailsComponent } from './note-details/note-details.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+
+const appRoutes: Routes = [
+  {
+    path: 'notes',
+    component: NotesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: NotesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'note-details/:id',
+    component: NoteDetailsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    NoteDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
